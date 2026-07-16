92776ce64fc7491918b6c963af0235913821a4b7
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2026-07-16 22:44:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (37.72%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int func(int n, vector<int> &dp) {
-        if(n < 0) return 0;
-        if(n == 0) return 1;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = func(n-1, dp) + func(n-2, dp);
-    }
     int climbStairs(int n) {
-        // Memoization approach - t.c -> O(n), s.c -> O(n) + O(n) {dp array + recursive stack space}
-        vector<int> dp(n+1, -1);
-        return func(n, dp);
+        // using tabulation - O(n) t.c
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
