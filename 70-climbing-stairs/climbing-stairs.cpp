b85d0ddd05841fed0d0ef263dfa73c9dba19cf73
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2026-07-16 22:46:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (88.20%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        // using tabulation - O(n) t.c
-        vector<int> dp(n+1, 0);
-        dp[0] = 1;
-        dp[1] = 1;
+        if(n == 1) return 1;
+        // using space optimization - O(n) t.c, O(1) s.c
+        int f = 1;
+        int s = 1;
+        int t = 0;
         for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            t = f+s;
+            f = s;
+            s = t;
         }
 
-        return dp[n];
+        return t;
     }
 };
