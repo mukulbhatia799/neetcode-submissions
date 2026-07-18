61f699f965d04050cd54afaaf125a4a9a1d5a5ef
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2026-07-18 11:44:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (64.41%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -5,13 +5,14 @@ public:
             return 0;
         }
         if(dp[ind] != -1) return dp[ind];
-        int take = nums[ind] + findAns(nums, dp, ind+2);
-        int notTake = findAns(nums, dp, ind+1);
+        int take = 0, notTake = 0;
+        take = nums[ind] + findAns(nums, dp, ind+2);
+        notTake = findAns(nums, dp, ind+1);
 
         return dp[ind] = max(take, notTake);
     }
     int rob(vector<int>& nums) {
-        // tabulation approach - O(n) t.c and O(n) s.c for dp array
+        // using memoization
         vector<int> dp(nums.size()+1, -1);
         return findAns(nums, dp, 0);
     }
