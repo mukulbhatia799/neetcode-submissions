14b0df65b5f8cc11b9e1a921783f81bd4b2d5688
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2026-07-18 22:43:10 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (91.29%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -4,20 +4,20 @@ public:
         // using Tabulation
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-        dp[0][0] = grid[0][0];
-        for(int i = 1; i < m; i++) dp[0][i] = grid[0][i] + dp[0][i-1];
+        vector<int> dp(m, 0);
+        dp[0] = grid[0][0];
+        for(int i = 1; i < m; i++) dp[i] = grid[0][i] + dp[i-1];
         
         for(int i = 1; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 int left = 1e5, top = 1e5;
-                if(j-1 >= 0) left = grid[i][j] + dp[i][j-1];
-                top = grid[i][j] + dp[i-1][j];
-                dp[i][j] = min(left, top);
+                if(j-1 >= 0) left = grid[i][j] + dp[j-1];
+                top = grid[i][j] + dp[j];
+                dp[j] = min(left, top);
             }
         }
 
-        return dp[n-1][m-1];
+        return dp[m-1];
 
     }
 };
