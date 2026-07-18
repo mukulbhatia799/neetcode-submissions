6ff4945c11801d43b2c9b890569ef0eb6b8bac8c
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2026-07-18 11:59:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.88%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,17 +3,17 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        // using tabulation
-        // The approach is to build the dp array which will contain the maximum money robbed 0...i at arr[i].
-        vector<int> dp(n, -1);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        // using space optimization
+        // just track the previous two and rest will follow-up while traversing.
+        int f = nums[0];
+        int s = max(nums[0], nums[1]);
         for(int i = 2; i < n; i++) {
-            int take = nums[i] + dp[i-2];
-            int notTake = dp[i-1];
-            dp[i] = max(take, notTake);
+            int take = nums[i] + f;
+            int notTake = s;
+            f = s;
+            s = max(take, notTake);
         }
 
-        return dp[n-1];
+        return s;
     }
 };
