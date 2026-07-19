4aab0140d76764d0255a396675538685444547a6
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2026-07-19 10:00:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (99.51%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        // brute force approach
         int n = nums.size();
         int ans = INT_MIN;
+
+        int mul = 1;
         for(int i = 0; i < n; i++) {
-            int mul = 1;
-            for(int j = i; j < n; j++) {
-                mul *= nums[j];
-                ans = max(ans, mul);
-            }
+            mul*=nums[i];
+            ans = max(ans, mul);
+            if(mul == 0) mul = 1;
         }
+        mul = 1;
+        for(int i = n-1; i >= 0; i--) {
+            mul*=nums[i];
+            ans = max(ans, mul);
+            if(mul == 0) mul = 1;
+        }
+
         return ans;
     }
 };
