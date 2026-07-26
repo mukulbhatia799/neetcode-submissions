dd26a8f2527a8b92e834986d1919594351ded65d
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2026-07-26 19:10:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (31.34%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        // two pointers approach - learned two new functions: isalnum() and tolower()
         int n = s.size();
         string phrase = "";
         for(int i = 0; i < n; i++) {
             if(isalnum(s[i])) phrase.push_back(tolower(s[i]));
         }
-        cout << "phrase: " << phrase << endl;
         int i = 0, j = phrase.size()-1;
         while(i < j) {
             if(phrase[i] != phrase[j]) return false;
