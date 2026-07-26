6136c7038df616c8b0df9bed02dcf4de4b2f3bd7
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2026-07-26 18:33:23 +0530

    Time: 7 ms (5.11%) | Memory: 40.2 MB (61.67%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        // using prefix sum approach
+        // using prefix sum approach (more optimal)
         int n = nums.size();
-        vector<int> ans;
-        vector<int> pref(n, 1);
-        vector<int> suff(n, 1);
+        vector<int> ans(n, 1);
         
         for(int i = 1; i < n; i++) {
-            pref[i] = pref[i-1] * nums[i-1];
+            ans[i] = ans[i-1] * nums[i-1];
         }
-        for(int i = n-2; i >= 0; i--) {
-            suff[i] = suff[i+1] * nums[i+1];
-        }
-        for(int i = 0; i < n; i++) {
-            ans.push_back(pref[i]*suff[i]);
+        int suff = 1;
+        for(int i = n-1; i >= 0; i--) {
+            ans[i] = ans[i] * suff;
+            suff *= nums[i];
         }
 
         return ans;
