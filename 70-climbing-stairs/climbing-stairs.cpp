fcd3129745c99f247b7274e34fcf8b7920c60912
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2026-08-06 17:44:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.90%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
+    int findAns(int n, int ind, vector<int>& dp) {
+        if(ind == n) return 1;
+        if(ind > n) return 0;
+        if(dp[ind] != -1) return dp[ind];
+        int ans = findAns(n, ind+1, dp);
+        ans += findAns(n, ind+2, dp);
+        return dp[ind] = ans;
+    }
     int climbStairs(int n) {
-        if(n == 1) return 1;
-        // using space optimization - O(n) t.c, O(1) s.c
-        int f = 1;
-        int s = 1;
-        int t = 0;
-        for(int i = 2; i <= n; i++) {
-            t = f+s;
-            f = s;
-            s = t;
-        }
-
-        return t;
+        // memoization
+        vector<int> dp(n+1, -1);
+        return findAns(n, 0, dp);
     }
 };
