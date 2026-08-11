442041edd27754de2650ea54c55e8a34c9278570
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2026-08-11 13:48:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (55.24%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,49 +1,25 @@
 class Solution {
 public:
-    int findAns(vector<vector<int>>& dp, int m, int n, int row, int col) {
-        if(row == m-1 && col == n-1) {
+    int findAns(int currRow, int currCol, int destRow, int destCol, vector<vector<int>>& dp) {
+        if(currRow == destRow-1 && currCol == destCol-1) {
             return 1;
         }
-        if(dp[row][col] != -1) return dp[row][col];
-        int count = 0;
-        if(row+1 < m) {
-            // bottom
-            count = findAns(dp, m, n, row+1, col);
-        }
-        if(col+1 < n) {
-            // right
-            count += findAns(dp, m, n, row, col+1);
-        }
 
-        return dp[row][col] = count;
+        if(dp[currRow][currCol] != -1) return dp[currRow][currCol];
+
+        int ways = 0;
+        if(currRow+1 < destRow) {   // bottom
+            ways = findAns(currRow+1, currCol, destRow, destCol, dp);
+        }
+        if(currCol+1 < destCol) {   // right
+            ways += findAns(currRow, currCol+1, destRow, destCol, dp);
+        }
+        return dp[currRow][currCol] = ways;
     }
     int uniquePaths(int m, int n) {
-        // using space optimization
-        vector<int> dp_1D(n);
-        // row1
-        for(int i = 0; i < n; i++) {
-            dp_1D[i] = 1;
-        }
+        // brute force approach - O(2^n)
+        vector<vector<int>> dp(m, vector<int>(n, -1));
 
-        // row2 to m
-        for(int i = 1; i < m; i++) {
-            int prev = 0;
-            for(int j = 0; j < n; j++) {
-                int paths = 0;
-                if(j!=0) paths = prev;
-                if(i!=0) paths += dp_1D[j];
-                dp_1D[j] = paths;
-                prev = paths;
-            }
-        }
-
-        // for(int i = 0; i < m; i++) {
-        //     for(int j = 0; j < n; j++) {
-        //         cout << dp[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
-
-        return dp_1D[n-1];
+        return findAns(0, 0, m, n, dp);
     }
 };
